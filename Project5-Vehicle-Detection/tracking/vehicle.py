e451fe0f298b37685867b9b8674ab9d9473eeba3
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-04-22 10:37:12 -0500

    add closest_colour method

--- Project5-Vehicle-Detection/tracking/vehicle.py
+++ Project5-Vehicle-Detection/tracking/vehicle.py
@@ -222,3 +222,15 @@ class Vehicle():
         self.selfie = self.takeProfileSelfie(self.maskedImage)
 
         return self.roadGrid
+
+    def closest_colour(self, requested_colour):
+        """ classify the vehicle by its main color components"""
+        min_colours = {}
+        for key, name in webcolors.css3_hex_to_names.items():
+            r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+            rd = (r_c - requested_colour[0]) ** 2
+            gd = (g_c - requested_colour[1]) ** 2
+            bd = (b_c - requested_colour[2]) ** 2
+            min_colours[(rd + gd + bd)] = name
+
+        return min_colours[min(min_colours.keys())]
